0c421c75bab92e69b5a38012555b6000245b6ad9
Quentin Richert <quentin@richert.co>
2023-08-11 19:57:43 +0200

    v1

---
+++ Makefile
@@ -0,0 +1,55 @@
+ERROR := \033[0;91m
+INFO := \033[0;94m
+NC := \033[0m
+
+define show_help_message
+	echo "Usage: make TARGET"
+	echo ""
+	echo "Commands:"
+	grep -hE '^[A-Za-z0-9_ \-]*?:.*##.*$$' $(MAKEFILE_LIST) | \
+	    awk 'BEGIN {FS = ":.*?## "}; {printf "  $(INFO)%-12s$(NC) %s\n", $$1, $$2}'
+endef
+
+define show_error_message
+	echo "$(ERROR)[Error] $(1)$(NC)"
+endef
+
+PREFIX ?= /usr/local
+
+SOURCE_DIRS := cronrunner tests
+
+.PHONY: all
+all: help
+
+.PHONY: help
+help: ## Show this help message
+	@$(show_help_message)
+
+.PHONY: l
+l: lint
+.PHONY: lint
+lint: ## Run various linting tools
+	@pre-commit run --all-files
+
+.PHONY: t
+t: test
+.PHONY: test
+test: ## Run unit tests
+	@python -m unittest
+
+.PHONY: c
+c: coverage
+.PHONY: coverage
+coverage: ## Unit tests coverage report
+	@python -m coverage run -m unittest
+	@python -m coverage html -d var/htmlcov
+	@open var/htmlcov/index.html || xdg-open var/htmlcov/index.html || :
+
+.PHONY: install
+install:
+	install -d $(PREFIX)/bin/
+	install ./cronrunner/cronrunner.py $(PREFIX)/bin/cronrunner
+
+%:
+	@$(call show_error_message,Unknown command '$@')
+	@$(show_help_message)
---
+++ cronrunner/cronrunner.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python3
+
+import re
+import subprocess
+import sys
+from dataclasses import dataclass
+
+
+class CrontabReadError(Exception):
+    def __init__(self, *args, exit_code: int = 1, detail: str = "") -> None:
+        self.exit_code: int = exit_code
+        self.detail: str = detail
+        super().__init__(*args)
+
+
+class _CrontabReader:
+    @staticmethod
+    def read() -> str:
+        try:
+            process: subprocess.CompletedProcess = subprocess.run(
+                ["crontab", "-l"],
+                capture_output=True,
+                text=True,
+                check=True,
+            )
+        except subprocess.CalledProcessError as e:
+            raise CrontabReadError(
+                "Cannot read crontab of current user.",
+                exit_code=e.returncode,
+                detail=e.stderr,
+            )
+        except FileNotFoundError:
+            raise CrontabReadError("Unable to locate crontab executable on the system.")
+        return process.stdout
+
+
+@dataclass
+class CronJob:
+    schedule: str
+    job: str
+    description: str
+
+
+@dataclass
+class Variable:
+    value: str
+
+
+@dataclass
+class Comment:
+    value: str
+
+
+@dataclass
+class UnknownOrEmpty:
+    value: str
+
+
+class _CrontabParser:
+    def parse(self, crontab: str) -> list:
+        res: list = []
+        line: str
+        for line in crontab.splitlines():
+            line = line.strip()
+            if self._is_job(line):
+                schedule, job = self._split_schedule_and_job(line)
+                description: str = ""
+                if res and isinstance(res[-1], Comment):
+                    description = res[-1].value[1:].lstrip(" ")
+                res.append(CronJob(schedule, job, description))
+            elif self._is_variable(line):
+                res.append(Variable(line))
+            elif self._is_comment(line):
+                res.append(Comment(line))
+            else:
+                res.append(UnknownOrEmpty(line))
+
+        return res
+
+    @staticmethod
+    def _is_job(line: str) -> bool:
+        return bool(re.match(r"(\d+|\*|@)", line))
+
+    @staticmethod
+    def _split_schedule_and_job(line: str) -> tuple:
+        """Split schedule and job parts of a job line.
+
+        This is a naive splitter that assumes a schedule consists of
+        either one element if it is a shortcut (e.g., @daily), or five
+        elements if not (e.g., * * * * *, 0 12 * * *, etc.).
+
+        Once the appropriate number of elements is consumed (i.e., the
+        schedule is consumed), it considers the rest to be the job
+        itself.
+        """
+        schedule_length: int = 1 if line.startswith("@") else 5
+        schedule: list = []
+        job: list = []
+        i: int = 0
+        for element in line.split(" "):
+            # Schedule
+            if i < schedule_length:
+                schedule.append(element)
+                if element:
+                    i += 1
+            # Job
+            else:
+                job.append(element)
+        schedule: str = " ".join(schedule).strip()
+        job: str = " ".join(job).strip()
+        return schedule, job
+
+    @staticmethod
+    def _is_variable(line: str) -> bool:
+        return bool(re.match(r"[a-zA-Z_][a-zA-Z0-9_]*", line))
+
+    @staticmethod
+    def _is_comment(line: str) -> bool:
+        return line.startswith("#")
+
+
+@dataclass
+class Crontab:
+    nodes: list
+
+    def __bool__(self) -> bool:
+        return len(self.jobs) > 0
+
+    @property
+    def jobs(self) -> list:
+        return [node for node in self.nodes if isinstance(node, CronJob)]
+
+    def run(self, job: CronJob) -> None:
+        if job not in self.nodes:
+            raise ValueError(f"Unknown job: {job}.")
+        out: list = self._extract_variables_and_target_job(job)
+        subprocess.run(["bash", "-c", ";".join(out)])
+
+    def _extract_variables_and_target_job(self, job: CronJob) -> list:
+        out: list = []
+        for node in self.nodes:
+            if isinstance(node, Variable):
+                out.append(node.value)
+            if node == job:
+                out.append(node.job)
+                break  # Variables coming after the job are not used.
+        return out
+
+
+def get_crontab() -> Crontab:
+    crontab: str = _CrontabReader().read()
+    nodes: list = _CrontabParser().parse(crontab)
+    return Crontab(nodes)
+
+
+def color_green(string: str) -> str:
+    return "\033[0;92m{}\033[0m".format(string)
+
+
+def color_error(string: str) -> str:
+    return "\033[0;91m{}\033[0m".format(string)
+
+
+def color_highlight(string: str) -> str:
+    return "\033[0;94m{}\033[0m".format(string)
+
+
+def color_attenuate(string: str) -> str:
+    return "\033[0;90m{}\033[0m".format(string)
+
+
+def main() -> int:
+    try:
+        crontab: Crontab = get_crontab()
+    except CrontabReadError as e:
+        print(color_error(str(e)))
+        if e.detail:
+            print(e.detail)
+        return e.exit_code
+
+    if not crontab:
+        print("No jobs to run.")
+        return 0
+
+    for i, job in enumerate(crontab.jobs):
+        description: str = (
+            f"{color_highlight(job.description)} " if job.description else ""
+        )
+        schedule: str = color_attenuate(job.schedule)
+        command: str = job.job
+        print(f"{i + 1}) {description}{schedule} {command}")
+
+    job_number: int = 0
+    while True:
+        try:
+            job_number: int = int(input("Select a job to run> "))
+            if not 0 < job_number <= len(crontab.jobs):
+                raise ValueError
+        except ValueError:
+            print(color_error("Invalid job number."))
+            continue
+        break
+
+    job: CronJob = crontab.jobs[job_number - 1]
+    print(color_green("$"), job.job)
+    crontab.run(job)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
